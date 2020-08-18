81795895c01f57860c12af1cf3d8b2b30f829265
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-18 11:48:50 -0700

    Create vowel replacement in JS.
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
   $("#formPuzzle").submit(function(event) {
     event.preventDefault();
     let puzzleResult = $("#puzzle1").val();
+let
+    string = puzzleResult.replace( /[aeiouAEIOU]/g, '-' );
+    console.log(string);
 
   });
 });
